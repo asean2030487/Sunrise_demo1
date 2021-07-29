c924a8367726011e544a17a494896e754a950a1b
Sean You <asean88520@gmail.com>
2021-07-29 20:04:59 +0800

    首頁替換

--- src/router/index.js
+++ src/router/index.js
@@ -7,31 +7,29 @@ import VueAxios from 'vue-axios'
 Vue.use(VueRouter)
 Vue.use(VueAxios, axios)
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/Demo',
-    name: 'Demo',
-    component: () => import('@/views/Demo/Demo1.vue')
-  }
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/',
+        name: 'Demo',
+        component: () => import('@/views/Demo/Demo1.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 export default router
-
-
